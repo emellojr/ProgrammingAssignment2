89d22be89c592584980f285d7247e3bbeca28154
Elias Junior <elias.mellojr@gmail.com>
2015-03-22 00:57:50 -0300

    Final Version of cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Created for: Elias Junior from Brazil
+## Date: 21/03/2015
+## This code fetch the inverse matrix using cache for better performance
 
-## Write a short comment describing this function
+## This function stores the value of my matrix in 
+## a variable, and create methods to assign and take 
+## the value of my inverse matrix.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(mymatrix = matrix()) {
+			invm <- NULL
+            setmymatrix <- function(y) {
+                    invm <<- NULL
+					mymatrix <<- y
+            }
+            getmymatrix <- function() mymatrix
+            setmyInvmatrix <- function(solve) invm <<- solve
+            getmyInvmatrix <- function() invm
+            list(setmymatrix = setmymatrix, 
+				 getmymatrix = getmymatrix,
+                 setmyInvmatrix = setmyInvmatrix,
+                 getmyInvmatrix = getmyInvmatrix)
 }
 
-
-## Write a short comment describing this function
+## In this function I use the methods / functions previously 
+## created to store the value in the cache and perform the 
+## inversion of the matrix stored in my global variable
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+		invm <- x$getmyInvmatrix()
+            if(!is.null(invm)) {
+                    message("getting cached data")
+                    return(invm)	
+            }
+            data <- x$getmymatrix()
+            invm <- solve(data, ...)
+            x$setmyInvmatrix(invm)
+			invm
 }
